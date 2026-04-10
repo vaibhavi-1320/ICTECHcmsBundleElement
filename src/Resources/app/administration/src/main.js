1e254909668fbf134dc54149c3962f49e74d7445
vaibhavi-ict <vaibhavi@icreativetechnologies.com>
2026-04-10 17:58:39 +0530

    add all changes

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -3,37 +3,42 @@ import enGB from './snippet/en-GB.json';
 
 import './module/sw-cms/elements/ict-two-column';
 import './module/sw-cms/elements/ict-three-column';
-import './module/sw-cms/elements/ict-two-column';
+import './module/sw-cms/elements/ict-four-column';
 import './module/sw-cms/elements/ict-five-column';
 import './module/sw-cms/elements/ict-six-column';
+import './module/sw-cms/elements/ict-seven-column';
+import './module/sw-cms/elements/ict-eight-column';
 import './module/sw-cms/elements/ict-nine-column';
-import './module/sw-cms/blocks/ict-three-column';
-import './module/sw-cms/blocks/ict-four-column';
-import './module/sw-cms/blocks/ict-two-column';
-import './module/sw-cms/blocks/ict-five-column';
-import './module/sw-cms/blocks/ict-six-column';
-import './module/sw-cms/blocks/ict-nine-column';
+import './module/sw-cms/elements/ict-ten-column';
+import './module/sw-cms/elements/ict-eleven-column';
+import './module/sw-cms/elements/ict-twelve-column';
+<<<<<<< HEAD
 import './module/sw-cms/elements/ict-four-column';
 import './module/sw-cms/elements/ict-seven-column';
 import './module/sw-cms/elements/ict-eight-column';
 import './module/sw-cms/elements/ict-ten-column';
 import './module/sw-cms/elements/ict-eleven-column';
 import './module/sw-cms/elements/ict-twelve-column';
-import './module/sw-cms/elements/ict-four-column';
-import './module/sw-cms/elements/ict-seven-column';
-import './module/sw-cms/elements/ict-eight-column';
-import './module/sw-cms/elements/ict-ten-column';
-import './module/sw-cms/elements/ict-eleven-column';
-import './module/sw-cms/elements/ict-twelve-column';
+=======
+>>>>>>> c4b2371c438a98274b1e7d1abf6d3b799bf88f8f
 
 import './module/sw-cms/blocks/ict-two-column';
 import './module/sw-cms/blocks/ict-three-column';
 import './module/sw-cms/blocks/ict-four-column';
+<<<<<<< HEAD
+=======
+import './module/sw-cms/blocks/ict-five-column';
+import './module/sw-cms/blocks/ict-six-column';
+>>>>>>> c4b2371c438a98274b1e7d1abf6d3b799bf88f8f
 import './module/sw-cms/blocks/ict-seven-column';
 import './module/sw-cms/blocks/ict-eight-column';
+import './module/sw-cms/blocks/ict-nine-column';
 import './module/sw-cms/blocks/ict-ten-column';
 import './module/sw-cms/blocks/ict-eleven-column';
 import './module/sw-cms/blocks/ict-twelve-column';
+
+import './module/sw-cms/elements/ict-image-gallery';
+import './module/sw-cms/blocks/ict-image-gallery';
 
 Shopware.Locale.extend('de-DE', deDE);
 Shopware.Locale.extend('en-GB', enGB);
---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/ict-image-gallery/component/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-block-ict-image-gallery.html.twig';
+import './sw-cms-block-ict-image-gallery.scss';
+
+export default {
+    template,
+
+    compatConfig: Shopware.compatConfig,
+};
